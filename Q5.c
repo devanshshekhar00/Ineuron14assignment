2598e23df25dd18b0d532ccc2c46c56f06499156
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-08-08 16:31:11 +0530

    Create Q5.c

---
+++ Q5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    int i = 0;
+    while(i < 10)
+    {
+        printf("Enter element\t");
+        scanf("%d",&arr[i]);
+        i++;
+    }
+    for(int i=0;i<9;i++)
+    {
+        for(int j = 0 ; j < 9;j++)
+        {
+            if(arr[j] > arr[j+1])
+        {
+            int k = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = k;
+        }
+        }
+        
+    }
+    printf("Smallest  number is %d",arr[0]);
+
+
+    return 0;
+}
