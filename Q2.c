8ee1a0d553c02ff52f998b5c320519d4b63aac35
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-08-08 18:22:39 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int arr[10];double sum = 0;
+    int arr[10];double avg = 0;
     printf("Enter 10 elements\n");
     for(int i = 0 ; i < 10;i++)
     {
         scanf("%d",&arr[i]);
-        sum = sum + arr[i];
+        avg = avg + arr[i];
         printf("Enter another element\n");
     }
-    printf("%f",sum/10);
+    printf("%f",avg/10);
     
 
     return 0;
